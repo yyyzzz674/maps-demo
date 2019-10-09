1d79c94a5146bf7532a4ee9778b227a1e7a97e38
yyyzzz674 <yz624557674@163.com>
2019-10-09 10:08:13 +0800

    高德地图api

---
+++ src/views/amap/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <map-drag class="mapbox" @drag="dragMap"></map-drag>
+  </div>
+</template>
+<script>
+import mapDrag from './components/mapDrag'
+export default {
+  name: 'AMap',
+  components: {
+    mapDrag
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.mapbox
+  height 400px
+  margin-bottom 20px
+  float left
+</style>
